e9d86c23abaa506b1a01cfef387dea46436c0bbb
CocoGir <corentin.giraud.23@eigsi.fr>
2022-05-03 12:56:12 +0200

    1 file modified, 2 deleted

--- src/lib/listing.js
+++ src/lib/listing.js
@@ -7,26 +7,4 @@ export const continents = [
 	{ nameC: 'Afrique', countries: [{ nameP: 'Kenya' }, { nameP: 'Bénin' }] }
 ];
 
-export const indicFra = [ {
-nameC: "France",
-langue: "Français",
-monnaie: "Euro",
-population: 67390000, 
-superficie: 543940,
-conduite: "droite",
-capitale: "Paris",
-villes: "Marseille, Lyon, Bordeaux, Toulouse"
-}]
-
-export const indicEsp = [ {
-	nameC: "Espagne",
-	langue: "Espagnol",
-	monnaie: "Euro",
-	population: 47350000, 
-	superficie: 505990,
-	conduite: "droite",
-	capitale: "Madrid",
-	villes: "Barcelone, Valence, Séville, Malaga"
-	}]
-
 ;
